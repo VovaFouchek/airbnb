fe8b1b38092e69de59345d6d7edc1639e893bf9d
Volodymyr Fouchek <vinvova555333@gmail.com>
2023-09-17 16:33:34 +0300

    Add Categories

---
+++ app/components/CategoryBox.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
+import { IconType } from 'react-icons';
+import queryString from 'query-string';
+
+interface CategoryBoxProps {
+  icon: IconType;
+  label: string;
+  selected?: boolean;
+}
+
+const CategoryBox: React.FC<CategoryBoxProps> = ({ icon: Icon, label, selected }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = queryString.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = queryString.stringifyUrl(
+      {
+        url: '/',
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, router, params]);
+
+  return (
+    // todo: Add key Event
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    <div
+      onClick={handleClick}
+      className={`
+        flex 
+        cursor-pointer 
+        flex-col 
+        items-center 
+        justify-center
+        gap-2
+        border-b-2
+        p-3
+        transition
+        hover:text-neutral-800
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+      `}
+    >
+      <Icon size={26} />
+      <div className="text-sm font-medium">{label}</div>
+    </div>
+  );
+};
+
+export default CategoryBox;
--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,13 +1,20 @@
 'use client';
 
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const Logo = () => {
-  // const router = useRouter();
+  const router = useRouter();
 
   return (
-    <Image alt="Logo" className="hidden cursor-pointer md:block" width="100" height="100" src="/images/logo.png" />
+    <Image
+      onClick={() => router.push('/')}
+      alt="Logo"
+      className="hidden cursor-pointer md:block"
+      width="100"
+      height="100"
+      src="/images/logo.png"
+    />
   );
 };
 
